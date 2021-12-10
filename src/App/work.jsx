da0845f3e14b0db2185a83cece825f014a0bcb8c
snehal-bagde <snehal.t.bagde@gmail.com>
2021-12-10 19:49:16 +0530

    Snehal Push

--- src/App/work.jsx
+++ src/App/work.jsx
@@ -8,33 +8,32 @@ export default function Work() {
     "MC5ZYUVtbXhNQUFETUFuODZN.77-977-9Ye-_ve-_vX8o77-977-977-977-977-9a--_ve-_ve-_ve-_ve-_ve-_vQITBF5h77-977-977-977-977-977-9TO-_vQ";
   // This is where you would add your access token for a Private repository
   const Client = Prismic.client(apiEndpoint, { accessToken });
-  const [doc, setDocData] = useState(null);
+  const [doc, setDoc] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await Client.query(
-        Prismic.Predicates.at("document.type", "worklist")
-      );
-      if (response) {
-        setDocData(response.results);
-        console.log("harshit", response.results);
-      }
-    };
     fetchData();
   }, []);
+
+  const fetchData = async () => {
+    const response = await Client.query(
+      Prismic.Predicates.at("document.type", "worklist")
+    );
+    if (response) {
+      setDoc(response.results);
+      console.log("harshit", response.results);
+    }
+  };
   return (
     <div className="container">
-      {
-      doc.map((e) => {
+      {doc?.map((e) => (
         <div className="project">
           <div className="project-image"></div>
           <h2 className="project-name">Project Name </h2>
           <p className="project-description">
             Description asdjad asshhee shahsdasdhb wahbdwah
           </p>
-        </div>;
-      })
-      }
+        </div>
+      ))}
     </div>
   );
 }
